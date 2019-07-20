0bb152ca7d7530ae2c20caafb200780306165abf
Carezz <pmclensky@gmail.com>
2019-07-20 23:42:44 +0300

    Updated threshold values.

--- bigint.h
+++ bigint.h
@@ -25,9 +25,9 @@ typedef uint64_t bigint_double;
 #define BIGINT_BIGENDIAN 1
 #define BIGINT_LITTLEENDIAN 0 
 
-/* Algorithm-specific */
-#define BIGINT_KARATSUBA_THRESHOLD 1024 // measured in bits
-#define BIGINT_COMBA_THRESHOLD 257 // measured in limbs
+/* Algorithm-specific (measured in bits) */
+#define BIGINT_KARATSUBA_THRESHOLD 2048 
+#define BIGINT_COMBA_THRESHOLD 512
 
 /* Bytes in a limb */
 #define BIL (sizeof(bigint_limb))
